a5519ccfd9cefd4af2df50d5acad3797139a4daf
DawnWogerman <99206850+DawnWogerman@users.noreply.github.com>
2022-06-19 22:23:16 -0600

    added 404 message

--- server.js
+++ server.js
@@ -55,10 +55,14 @@ app.get('/api/animals', (req, res) => {
     res.json(results);
 });
 
-app.get('/api/animas/:id', (req, res) =>{
+app.get('/api/animals/:id', (req, res) => {
     const result = findById(req.params.id, animals);
-    res.json(result);
-});
+    if(result){
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+   });
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
