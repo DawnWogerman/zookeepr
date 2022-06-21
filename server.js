9352701a4487b796a19fb6a50af19babde22a964
DawnWogerman <99206850+DawnWogerman@users.noreply.github.com>
2022-06-20 21:21:08 -0600

    added post and validation

--- server.js
+++ server.js
@@ -63,6 +63,22 @@ function createNewAnimal(body, animalsArray) {
   return animal;
 }
 
+function validateAnimal(animal) {
+  if(!animal.name || typeof animal.name !== 'string') {
+    return false;
+  }
+  if(!animal.species || typeof animal.species !== 'string'){
+    return false;
+  }
+  if(!animal.diet || typeof animal.diet !== 'string') {
+    return false;
+  }
+  if(!animal.personalityTraits || !Array.isArray(animal.personalityTraits)){
+    return false;
+  }
+  return true;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if(req.query){
@@ -80,13 +96,17 @@ app.get('/api/animals/:id', (req, res) => {
     }
    });
 
-app.post('/api/animals', (req, res) => {
-  req.body.id = animals.length.toString();
-  //aadd animal to json file
-  const animal = createNewAnimal(req.body, animals);
-
-  res.json(req.animal);
-});
+   app.post('/api/animals', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = animals.length.toString();
+    
+    if(!validateAnimal(req.body)){
+      res.status(400).send('The animal is not properly formatted');
+    } else {
+      const animal = createNewAnimal(req.body, animals);
+      res.json(animal);
+    }
+  });
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
