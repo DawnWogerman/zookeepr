53d844b08f3b5222fa6ff972436bfbf746dcc703
DawnWogerman <99206850+DawnWogerman@users.noreply.github.com>
2022-06-20 20:52:46 -0600

    added function

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
+const fs = require('fs');
+const path = require('path');
 const { animals } = require('./data/animals');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+//parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+//parse incoming JSON data
+app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -47,6 +53,16 @@ function findById(id, animalsArray){
     return result;
 }
 
+function createNewAnimal(body, animalsArray) {
+  const animal = body;
+  animalsArray.push(animal);
+  fs.writeFileSync(
+    path.join(__dirname, './data/animals.json'),
+    JSON.stringify({ animals: animalsArray }, null, 2)
+  );
+  return animal;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if(req.query){
@@ -65,9 +81,11 @@ app.get('/api/animals/:id', (req, res) => {
    });
 
 app.post('/api/animals', (req, res) => {
-  //req.body is where our incoming content will be
-  console.log(req.body);
-  res.json(req.body);
+  req.body.id = animals.length.toString();
+  //aadd animal to json file
+  const animal = createNewAnimal(req.body, animals);
+
+  res.json(req.animal);
 });
 
 app.listen(PORT, () => {
